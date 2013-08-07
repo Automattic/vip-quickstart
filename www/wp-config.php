76e721f4130646c5a88569152938f2b98d1c6097
Josh Betz <j@joshbetz.com>
2013-08-06 22:47:38 -0500

    Nginx rewrite rules to enable multisite installed in a subdirectory

--- www/wp-config.php
+++ www/wp-config.php
@@ -100,7 +100,7 @@ define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
 define('DOMAIN_CURRENT_SITE', 'wp.dev');
-define('PATH_CURRENT_SITE', '/wp/');
+define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 
