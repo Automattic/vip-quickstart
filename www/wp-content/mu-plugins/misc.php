2252f9799072a87908f73b1348d24b6ce10b9472
Josh Betz <j@joshbetz.com>
2013-09-12 22:48:53 -0500

    Hide custom fields metabox
    Fixes #19

--- www/wp-content/mu-plugins/misc.php
+++ www/wp-content/mu-plugins/misc.php
@@ -2,7 +2,7 @@
 
 header( "X-hacker: If you're reading this, you should visit automattic.com/jobs and apply to join the fun, mention this header." );
 
-
+// Hide Plugins menu
 add_action( 'admin_menu', function() {
 	global $_wp_menu_nopriv;
 
@@ -10,4 +10,9 @@ add_action( 'admin_menu', function() {
 
 	if ( ! current_user_can( 'manager_network' ) )
 		$_wp_menu_nopriv['plugins.php'] = true;
+});
+
+// Hide Custom Fields metabox
+add_action( 'do_meta_boxes', function() {
+	remove_meta_box( 'postcustom', get_post_type(), 'normal' );
 });
