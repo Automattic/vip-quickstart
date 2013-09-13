e484f5de2ebf6e63330949e0d95f0960bbf23b60
Josh Betz <j@joshbetz.com>
2013-09-13 08:46:56 -0500

    Remove unnecessary caps check
    @mjangda noticed a typo. The reason this worked when I tested it is that
    we don't even need this code to completely hide and disable the plugins
    page for everyone who's not a superadmin. `remove_menu_page()` takes
    care of everything.

--- www/wp-content/mu-plugins/misc.php
+++ www/wp-content/mu-plugins/misc.php
@@ -4,12 +4,7 @@ header( "X-hacker: If you're reading this, you should visit automattic.com/jobs 
 
 // Hide Plugins menu
 add_action( 'admin_menu', function() {
-	global $_wp_menu_nopriv;
-
 	remove_menu_page( 'plugins.php' );
-
-	if ( ! current_user_can( 'manager_network' ) )
-		$_wp_menu_nopriv['plugins.php'] = true;
 });
 
 // Hide Custom Fields metabox
