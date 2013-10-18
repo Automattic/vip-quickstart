fcc56ae9e721baaccc250b9e6965e525d2a037aa
Josh Betz <j@joshbetz.com>
2013-10-18 15:01:13 -0500

    Reorg WordPress puppet script
    Move the WordPress installation stuff to the top because it's probably
    going to change more and is more directly relevant. The WP-CLI stuff
    and other utility ops that aren't going to change very often are moving
    to the bottom.
    
    Also, auto-installed plugins are now listed in an array. Just add the
    plugin slug to the `$plugins` array to auto-install it.
    
    Adds mrss - fixes #38

--- puppet/manifests/sections/wp.pp
+++ puppet/manifests/sections/wp.pp
@@ -1,23 +1,4 @@
-class { wp::cli:
-	ensure => installed,
-	install_path => '/vagrant/www/wp-cli',
-	version => '0.12.0'
-}
-
-exec { "rsync wp-content":
-	command => "rsync -a /vagrant/www/wp/wp-content/ /vagrant/www/wp-content",
-	onlyif => "/usr/bin/test -d /vagrant/www/wp-content"
-}
-
-exec { 'rm -rf /vagrant/www/wp/wp-content':
-	require => Exec['rsync wp-content'],
-	onlyif => '/usr/bin/test -d /vagrant/www/wp/wp-content'
-}
-
-file { 'local-config.php':
-	ensure => present,
-	path   => '/vagrant/www/local-config.php'
-}
+$plugins = ['developer', 'jetpack', 'mrss']
 
 # Install WordPress
 exec {"wp install /vagrant/www/wp":
@@ -26,26 +7,48 @@ exec {"wp install /vagrant/www/wp":
 	require => Class['wp::cli']
 }
 
+# Install plugins
+wp::plugin { $plugins:
+	location    => '/vagrant/www/wp',
+	networkwide => true,
+	require => Exec['wp install /vagrant/www/wp']
+}
+
+# Install VIP recommended developer plugins
+wp::command { 'developer install-plugins':
+	command  => 'developer install-plugins --type=wpcom-vip',
+	location => '/vagrant/www/wp',
+	require  => Wp::Plugin['developer']
+}
+
+# Update all the plugins
 wp::command { 'plugin update --all':
 	command  => 'plugin update --all',
 	location => '/vagrant/www/wp',
 	require => Exec['wp install /vagrant/www/wp']
 }
 
-wp::plugin { 'developer':
-	location    => '/vagrant/www/wp',
-	networkwide => true,
-	require => Exec['wp install /vagrant/www/wp']
+# Install WP-CLI
+class { wp::cli:
+	ensure => installed,
+	install_path => '/vagrant/www/wp-cli',
+	version => '0.12.0'
 }
 
-wp::plugin { 'jetpack':
-	location    => '/vagrant/www/wp',
-	networkwide => true,
-	require => Exec['wp install /vagrant/www/wp']
+# Sync wp-content
+exec { "rsync wp-content":
+	command => "rsync -a /vagrant/www/wp/wp-content/ /vagrant/www/wp-content",
+	onlyif => "/usr/bin/test -d /vagrant/www/wp-content"
 }
 
-wp::command { 'developer install-plugins':
-	command  => 'developer install-plugins --type=wpcom-vip',
-	location => '/vagrant/www/wp',
-	require  => Wp::Plugin['developer']
+# Remove wp-content from wp root
+exec { 'rm -rf /vagrant/www/wp/wp-content':
+	require => Exec['rsync wp-content'],
+	onlyif => '/usr/bin/test -d /vagrant/www/wp/wp-content'
+}
+
+# Create a local config
+file { 'local-config.php':
+	ensure => present,
+	path   => '/vagrant/www/local-config.php'
 }
