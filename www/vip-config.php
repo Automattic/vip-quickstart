8e97246fda73eb0dd99a543fc071f2371968f983
Josh Betz <j@joshbetz.com>
2013-10-19 11:45:41 -0500

    Move WordPress.com compat stuff to vip-config.php

---
+++ www/vip-config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Disable unfiltered HTML; this a default in Multisite, but we should extra enforce it
+ */
+define( 'DISALLOW_UNFILTERED_HTML', true );
+
+/**
+ * Disable file editing and WordPress updates
+ */
+define('DISALLOW_FILE_MODS', true);
