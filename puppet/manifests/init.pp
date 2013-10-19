c153a4045128f35842725244a0e888b66f480c88
Josh Betz <j@joshbetz.com>
2013-10-18 20:29:27 -0500

    Add a WP-CLI config file
    It's nice to be able to use WP-CLI anywhere inside the VM, so we're
    adding a config file with the path. In order to use the config file
    everywhere, I've also added an alias for the `wp` command with
    the full path to the config
    
    Fixes #15

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -20,3 +20,9 @@ exec { 'setup hosts':
 	command => 'sudo printf "\n# VIP Quickstart\n127.0.0.1 vip.dev\n" | sudo tee -a /etc/hosts',
 	unless => 'cat /etc/hosts | grep vip.dev'
 }
+
+# Setup bash aliases
+file { '/home/vagrant/.bash_aliases':
+   ensure => 'link',
+   target => '/vagrant/bin/dotfiles/quickstart_aliases',
+}
