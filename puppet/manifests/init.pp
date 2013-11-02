755544863caef096ec48ed704c0a2ddaf53c81bf
Josh Betz <j@joshbetz.com>
2013-11-02 18:01:39 -0500

    Upgrade subversion version

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -44,3 +44,19 @@ exec { 'configure postfix hostname':
 	user => root,
 	notify => Service['postfix']
 }
+
+define line($file, $line, $ensure = 'present') {
+    case $ensure {
+        default : { err ( "unknown ensure value ${ensure}" ) }
+        present: {
+            exec { "/bin/echo '${line}' >> '${file}'":
+                unless => "/bin/grep -qFx '${line}' '${file}'"
+            }
+        }
+        absent: {
+            exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
+                onlyif => "/bin/grep -qFx '${line}' '${file}'"
+            }
+        }
+    }
+}
