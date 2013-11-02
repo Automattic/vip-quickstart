23d515d16b2b280b170aba9382a8be12f3baa211
Josh Betz <j@joshbetz.com>
2013-11-02 18:04:42 -0500

    Clean up puppet manifests

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,28 +1,6 @@
 Exec { path => '/bin:/usr/bin:/usr/local/bin:/usr/sbin:/sbin' }
 
-# Make sure apt-get is up-to-date before we do anything else
-stage { 'updates': before => Stage['main'] }
-class { 'updates': stage => updates }
-class updates {
-	exec { 'apt-get update':
-		command => 'apt-get update --quiet --yes',
-		timeout => 0
-	}
-}
-
 import 'sections/*'
-
-# Additional packages
-package { 'postfix': ensure => present }
-service { 'postfix': ensure => running }
-
-# Install / update ClamAV for use w/ VIP Scanner
-# package { 'clamav': ensure => present }
-
-# exec { 'update clamav db':
-# 	command => 'sudo freshclam',
-# 	require => Package['clamav']
-# }
 
 # Set vip.dev in hosts file:
 exec { 'setup hosts':
@@ -34,29 +12,4 @@ exec { 'setup hosts':
 file { '/home/vagrant/.bash_aliases':
    ensure => 'link',
    target => '/vagrant/bin/dotfiles/quickstart_aliases',
-}
-
-
-# Configure postfix
-exec { 'configure postfix hostname':
-	command => 'sed -i "s/precise32/vip.dev/g" /etc/postfix/main.cf',
-	onlyif => 'cat /etc/postfix/main.cf | grep "precise32"',
-	user => root,
-	notify => Service['postfix']
-}
-
-define line($file, $line, $ensure = 'present') {
-    case $ensure {
-        default : { err ( "unknown ensure value ${ensure}" ) }
-        present: {
-            exec { "/bin/echo '${line}' >> '${file}'":
-                unless => "/bin/grep -qFx '${line}' '${file}'"
-            }
-        }
-        absent: {
-            exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
-                onlyif => "/bin/grep -qFx '${line}' '${file}'"
-            }
-        }
-    }
 }
