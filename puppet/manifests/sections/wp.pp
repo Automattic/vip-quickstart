f32ba2c6e044c1b4de823ee02b5ace1af36f8782
Josh Betz <j@joshbetz.com>
2013-11-02 13:02:40 -0500

    Let the VM handle SVN checkouts if the host can't
    1. Ask for a WordPress.com username and password
    2. Set facter facts with the SVN username and password
    3. If the directories don't exist, do a checkout using the auth
    that we collected (for the shared plugins repo)
    
    This only does the SVN checkout if the directory doesn't exist.
    In the case of the shared plugins repo, it also requires that
    we give it a username and password.
    
    The SVN up only happens if `svn info` returns succesful. If it's
    not an SVN repo or a different version of SVN was used to do the
    checkout, the exit code won't be 0 so we won't try to `svn up`.
    
    Note: If the username and password were wrong, it's going to cause
    stuf to fail. We should probably introduce better error handling
    
    see #31, #58

--- puppet/manifests/sections/wp.pp
+++ puppet/manifests/sections/wp.pp
@@ -35,6 +35,41 @@ class { wp::cli:
 	version => '0.12.1'
 }
 
+stage { 'svn': before => Stage['main'] }
+class { 'svn': stage => svn }
+class svn {
+	package { 'subversion': ensure => present }
+
+	# SVN checkout WordPress
+	exec { 'checkout WordPress':
+		command => 'svn co http://core.svn.wordpress.org/trunk/ /vagrant/www/wp; svn rm --keep-local /vagrant/www/wp/wp-content',
+		unless => 'test -d /vagrant/www/wp && ls -A /vagrant/www/wp',
+		require => Package['subversion']
+	}
+
+	exec { 'svn up WordPress':
+		cwd => '/vagrant/www/wp',
+		command => 'svn up',
+		onlyif => 'svn info',
+		require => Exec['checkout WordPress']
+	}
+
+	# SVN checkout VIP plugins
+	exec { 'checkout plugins':
+		command => "svn co https://vip-svn.wordpress.com/plugins/ /vagrant/www/wp-content/themes/vip/plugins --username='${svn_username}' --password='${svn_password}' --non-interactive",
+		unless => "test -d/vagrant/www/wp-content/themes/vip/plugins && ls -A /vagrant/www/wp-content/themes/vip/plugins || test -z '${svn_username}' || test -z '${svn_password}'",
+		require => Package['subversion']
+	}
+
+	exec { 'svn up VIP plugins':
+		cwd => '/vagrant/www/wp-content/themes/vip/plugins',
+		command => "svn up --username='${svn_username}' --password='${svn_password}' --non-interactive",
+		unless => "test -z '${svn_username}' || test -z '${svn_password}'",
+		onlyif => 'svn info',
+		require => Exec['checkout plugins']
+	}
+}
+
 # Sync wp-content
 exec { "rsync wp-content":
 	command => "rsync -a /vagrant/www/wp/wp-content/ /vagrant/www/wp-content",
