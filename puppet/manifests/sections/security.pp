c7d701e7b2e94047835907292317cf66151724ff
Josh Betz <j@joshbetz.com>
2013-11-23 10:46:03 -0600

    Add UFW
    Fixes #77

---
+++ puppet/manifests/sections/security.pp
@@ -0,0 +1,13 @@
+include ufw
+
+ufw::limit { 22: }
+
+ufw::allow { "allow-all-http":
+    port => 80,
+    ip => 'any',
+}
+
+ufw::allow { "allow-dns-over-udp":
+    port => 53,
+    proto => "udp",
+}
