70b9def9ba855f76d80a91e66d942c1428ff207d
Josh Betz <j@joshbetz.com>
2013-11-25 15:07:44 -0600

    Add a dumb hack to fix nginx errors

--- puppet/manifests/sections/updates.pp
+++ puppet/manifests/sections/updates.pp
@@ -6,4 +6,11 @@ class updates {
         command => 'apt-get update --quiet --yes',
         timeout => 0
     }
+
+    # Dumb hack because we're not syncing the logs directory anymore
+    # TODO: Remove this for 1.0
+    file { '/var/log/nginx':
+        ensure => directory,
+        owner => 'root',
+    }
 }
