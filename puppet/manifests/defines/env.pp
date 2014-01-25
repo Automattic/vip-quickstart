78f3610dc35db093a002fcf527cde63637b24e3c
Josh Betz <j@joshbetz.com>
2014-01-25 14:38:01 -0600

    Set environment variables with Puppet
    Closes #109

---
+++ puppet/manifests/defines/env.pp
@@ -0,0 +1,6 @@
+define env($value) {
+    line { $title:
+        file => '/etc/environment',
+        line => "$title='$value'",
+    }
+}
--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,17 +1,4 @@
 Exec { path => '/bin:/usr/bin:/usr/local/bin:/usr/sbin:/sbin' }
 
+import 'defines/*'
 import 'sections/*'
-
-# Set vip.dev in hosts file:
-exec { 'setup hosts':
-	command => 'sudo printf "\n# VIP Quickstart\n127.0.0.1 vip.dev\n" | sudo tee -a /etc/hosts',
-	unless => 'cat /etc/hosts | grep vip.dev'
-}
-
-# Setup environment vars
-file { "/etc/environment":
-	content => inline_template('
-		WP_CLI_CONFIG_PATH="/srv/www/wp-cli.yml"
-		WP_TESTS_DIR="/srv/www/wp-tests/tests/phpunit/"
-	')
-}
