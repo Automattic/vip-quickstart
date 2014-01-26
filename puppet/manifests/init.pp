e67c12b310b27a9328fd64d82a383870a24c819a
Josh Betz <j@joshbetz.com>
2014-01-26 17:05:29 -0600

    Housekeeping: Move apt-get update to init

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -2,3 +2,13 @@ Exec { path => '/bin:/usr/bin:/usr/local/bin:/usr/sbin:/sbin' }
 
 import 'defines/*'
 import 'sections/*'
+
+# Make sure apt-get is up-to-date before we do anything else
+stage { 'updates': before => Stage['main'] }
+class { 'updates': stage => updates }
+class updates {
+    exec { 'apt-get update':
+        command => 'apt-get update --quiet --yes',
+        timeout => 0
+    }
+}
--- puppet/manifests/sections/updates.pp
+++
@@ -1,9 +0,0 @@
-# Make sure apt-get is up-to-date before we do anything else
-stage { 'updates': before => Stage['main'] }
-class { 'updates': stage => updates }
-class updates {
-    exec { 'apt-get update':
-        command => 'apt-get update --quiet --yes',
-        timeout => 0
-    }
-}
