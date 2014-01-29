ec543557a2cc5727a1e5530e43b58d9d87b14f6f
Josh Betz <j@joshbetz.com>
2014-01-29 16:07:07 -0600

    Enable custom domains
    See #94

--- puppet/manifests/sections/mail.pp
+++ puppet/manifests/sections/mail.pp
@@ -2,7 +2,7 @@
 package { 'postfix': ensure => present }
 service { 'postfix': ensure => running }
 exec { 'configure postfix hostname':
-    command => 'sed -i "s/precise32/vip.dev/g" /etc/postfix/main.cf',
+    command => "sed -i 's/precise32/$quickstart_domain/g' /etc/postfix/main.cf",
     onlyif => 'cat /etc/postfix/main.cf | grep "precise32"',
     user => root,
     notify => Service['postfix']
