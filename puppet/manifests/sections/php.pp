5f90964c5810f6f207c5fe3f41b3acc9232a5ff8
Phil Crumm <pcrumm@p3net.net>
2014-01-28 19:51:19 -0800

    Use PHP 5.4 to Match WordPress.com's Environment
    To better mirror WordPress.com's deployment environment, utilize PHP 5.4 instead of PHP 5.3. Ubuntu 12.04 does not include PHP 5.4.x in any of its official repositories, so a third party PPA must be used.

--- puppet/manifests/sections/php.pp
+++ puppet/manifests/sections/php.pp
@@ -1,11 +1,29 @@
+# Use PHP 5.4.24 like WordPress.com
+$php_version = '5.4.24-1+sury.org~precise+1'
+
 include php
+include apt
+
+
+apt::source { 'php54':
+	location => 'http://ppa.launchpad.net/ondrej/php5-oldstable/ubuntu',
+	release => 'precise',
+	repos => 'main',
+	key => '14aa40ec0831756756d7f66c4f4ea0aae5267a6c',
+	key_server => 'keyserver.ubuntu.com',
+	include_src => true
+}
 
 class {
 	'php::cli':
-		provider => 'apt';
+		provider => 'apt',
+		ensure => $php_version,
+		require => Apt::Source['php54'];
 	'php::composer':;
 	'php::dev':
-		provider => 'apt';
+		provider => 'apt',
+		ensure => $php_version,
+		require => Apt::Source['php54'];
 	'php::fpm':;
 	'php::pear':;
 	'php::phpunit':;
