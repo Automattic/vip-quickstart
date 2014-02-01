c1cd2779b93c4d0e15cfb54f4a054cbd26e46bcb
Josh Betz <j@joshbetz.com>
2014-01-31 19:15:27 -0600

    Bump types_hash_max_size to 2048 to fix Nginx on Digital Ocean

--- puppet/manifests/sections/webserver.pp
+++ puppet/manifests/sections/webserver.pp
@@ -1,7 +1,8 @@
 class { "nginx":
 	template => 'nginx/nginx.conf.erb',
 	disable_default_site => true,
-	client_max_body_size => '1024m'
+	client_max_body_size => '1024m',
+	types_hash_max_size => '2048',
 }
 
 nginx::vhost { "_":
