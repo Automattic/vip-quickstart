924a103099ff44556c54ef3c78323a9fec312fab
Josh Betz <j@joshbetz.com>
2014-02-03 14:24:41 -0600

    Use custom domain for admin's email

--- puppet/manifests/sections/wp.pp
+++ puppet/manifests/sections/wp.pp
@@ -2,7 +2,7 @@ $plugins = ['developer', 'jetpack', 'mrss']
 
 # Install WordPress
 exec {"wp install /srv/www/wp":
-	command => "/usr/bin/wp core multisite-install --url='$quickstart_domain' --title='$quickstart_domain' --admin_email='wordpress@vip.dev' --admin_name='wordpress' --admin_password='wordpress'",
+	command => "/usr/bin/wp core multisite-install --url='$quickstart_domain' --title='$quickstart_domain' --admin_email='wordpress@$quickstart_domain' --admin_name='wordpress' --admin_password='wordpress'",
 	cwd => '/srv/www/wp',
 	unless => "test -z $quickstart_domain",
 	require => [
