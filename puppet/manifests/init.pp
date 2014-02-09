a6de880a9a3ace1069e47a57d2089deedec6bc7a
Josh Betz <j@joshbetz.com>
2014-02-09 14:19:18 -0600

    Fix lint errors for init

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -6,6 +6,8 @@ import 'sections/*'
 # Make sure apt-get is up-to-date before we do anything else
 stage { 'updates': before => Stage['main'] }
 class { 'updates': stage => updates }
+
+# updates
 class updates {
     exec { 'apt-get update':
         command => 'apt-get update --quiet --yes',
