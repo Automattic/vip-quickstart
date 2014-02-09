027219d67bbe98ed8f77c20be64fba6ab487b0dc
Josh Betz <j@joshbetz.com>
2014-02-09 14:07:14 -0600

    Fix lint errors in mail manifest

--- puppet/manifests/sections/mail.pp
+++ puppet/manifests/sections/mail.pp
@@ -2,8 +2,8 @@
 package { 'postfix': ensure => present }
 service { 'postfix': ensure => running }
 exec { 'configure postfix hostname':
-    command => "sed -i 's/precise32/$quickstart_domain/g' /etc/postfix/main.cf",
-    onlyif => 'cat /etc/postfix/main.cf | grep "precise32"',
-    user => root,
-    notify => Service['postfix']
+    command => "sed -i 's/precise32/${quickstart_domain}/g' /etc/postfix/main.cf",
+    onlyif  => 'cat /etc/postfix/main.cf | grep "precise32"',
+    user    => root,
+    notify  => Service['postfix']
 }
