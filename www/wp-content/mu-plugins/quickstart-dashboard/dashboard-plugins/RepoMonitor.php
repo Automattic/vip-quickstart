97594cd5e0b881563fb202fdbd6fd06632c1161f
Mobius5150 <mike@michaelblouin.ca>
2014-02-14 16:21:58 -0700

    Adding RepoMonitor plugin and CLI tools.
    Added the `RepoMonitor` plugin that is responsible for monitoring all git
    and svn repos in quickstart and notifying the user when the repository is
    out of date.
    
    Added the basic repo monitoring and scanning functionality. SVN is not yet
    functional. Currently working on parsing as much information as possible
    from the git output.
    
    Added basic WP CLI tools for adding repos and scanning them for out of
    date repos. Does not yet support updating repos.

---
+++ www/wp-content/mu-plugins/quickstart-dashboard/dashboard-plugins/RepoMonitor.php
@@ -0,0 +1,158 @@
+<?php
+
+class RepoMonitor extends Dashboard_Plugin {
+
+	const REPO_CPT = 'qs_repo';
+
+	private $repos = null;
+
+	function __construct() {
+		// Register the dashboard plugin
+
+		// Setup cron
+	}
+
+	function name() {
+		return 'Repo Monitor';
+	}
+
+	function scan_repositories() {
+		foreach ( $this->get_repos() as $repo ) {
+			// Run the command to determine if it needs an update
+			if ( 'svn' == $repo['repo_type'] ) {
+				$results = $this->scan_svn_repo( $repo['repo_path'] );
+			}
+		}
+
+		return 0;
+	}
+
+	function get_repos() {
+		if ( is_null( $this->repos ) ) {
+			$this->load_repos();
+		}
+
+		return $this->repos;
+	}
+
+	private function load_repos() {
+		$args = array(
+			'orderby' => 'ID',
+			'order' => 'ASC',
+			'posts_per_page' => -1,
+			'post_type' => self::REPO_CPT,
+		);
+
+		$repo_query = get_posts( $args );
+
+		$this->repos = array();
+		foreach ( $repo_query as $repo ) {
+			$this->repos[] = array(
+				'repo_path' => get_post_meta( $repo->ID, 'qs_repo_path', true ),
+				'repo_type' => get_post_meta( $repo->ID, 'qs_repo_type', true ),
+				'repo_friendly_name' => $repo->post_title,
+				'warn_out_of_date' => get_post_meta( $repo->ID, 'qs_warn_out_of_date', true ),
+			);
+		}
+	}
+
+	function scan_svn_repo( $repo_path ) {
+		// Variables to load output into
+		$output = array();
+		$return_value = -1;
+
+		// Execute command
+		exec( 'svn status -u', $output, $return_value);
+
+		// Check return value
+		echo "	SVN command returned $return_value\n";
+
+		// Echo output
+		print_r( $output );
+
+		return array();
+	}
+
+	function scan_git_repo( $repo_path ) {
+		$cwd = getcwd();
+
+		// Variables to load output into
+		$output = array();
+		$return_value = -1;
+
+		// Go to repository directory
+		chdir( $repo_path );
+
+		// Start by updating remotes
+		exec( 'git remote update origin' );
+
+		// Now check the repo status
+		exec( 'git status -u no', $output, $return_value );
+
+		// Go back to the previous working directory
+		chdir( $cwd );
+
+		// Check return value
+		if ( 0 != $return_value ) {
+			print( 'Git command did not return 0' );
+			return;
+		}
+
+		// The second line of output will contain the repo status
+		$status = '';
+		foreach ( $output as $line ) {
+			$status .= trim( $line, '# ' ) . "\n";
+		}
+
+		return $this->parse_git_status_text( $status );
+	}
+
+	private function parse_git_status_text( $status ) {
+		$status_var = array(
+			'on_branch' => preg_match( "/on\Wbranch\W(?<branch>[a-zA-Z_-]+)/i", $status, $matches ) ? $matches['branch'] : '<unknown>',
+			'behind' => false,
+			'diverged' => false,
+		);
+
+		// Check if we're behind
+		if ( preg_match( "/your\Wbranch\Wis\Wbehind\W'(?<branch>[a-zA-Z\/]+)'\Wby\W(?<numcommits>\d+)\Wcommit/i", $status, $matches ) ) {
+			$status_var['behind'] = array(
+				'branch'	  => $matches['branch'],
+				'num_commits' => $matches['numcommits'],
+			);
+		}
+
+		return $status_var;
+	}
+
+	function add_repo( $args ) {
+		$defaults = array(
+			'repo_type'			 => 'git',
+			'repo_path'			 => '',
+			'repo_friendly_name' => '',
+			'warn_out_of_date'   => true,
+		);
+
+		$args = array_merge( $defaults, $args );
+
+		$post_args = array(
+			'post_type' => self::REPO_CPT,
+			'post_title' => $args['repo_friendly_name'],
+			'post_status' => 'publish',
+		);
+
+		$id = wp_insert_post( $post_args );
+
+		if ( is_wp_error( $id ) ) {
+			return $id;
+		}
+
+		add_post_meta( $id, 'qs_repo_type', $args['repo_type'], true );
+		add_post_meta( $id, 'qs_repo_path', $args['repo_path'], true );
+		add_post_meta( $id, 'qs_warn_out_of_date', $args['warn_out_of_date'], true );
+
+		$this->repos[] = $args;
+
+		return $id;
+	}
+}
--- www/wp-content/mu-plugins/quickstart-dashboard/includes/wp-cli.php
+++ www/wp-content/mu-plugins/quickstart-dashboard/includes/wp-cli.php
@@ -27,4 +27,93 @@ class Quickstart_Dashboard_CLI extends WP_CLI_Command {
 			WP_CLI::line( "	$name: {$plugin->name()}" );
 		}
 	}
+
+	/**
+	 * @subcommand scan_repos
+	 */
+	function scan_repos( $args, $assoc_args ) {
+		WP_CLI::line( 'Scanning Known Repositories...' );
+		
+		$repo_monitor = $this->load_repo_monitor();
+
+		if ( ! $repo_monitor ) {
+			return;
+		}
+
+		foreach ( $repo_monitor->get_repos() as $repo ) {
+			// Run the command to determine if it needs an update
+			WP_CLI::log( " Scanning {$repo['repo_type']} repo {$repo['repo_friendly_name']}...\n" );
+			if ( 'svn' == $repo['repo_type'] ) {
+				$results = $repo_monitor->scan_svn_repo( $repo['repo_path'] );
+			} elseif ( 'git' == $repo['repo_type'] ) {
+				$results = $repo_monitor->scan_git_repo( $repo['repo_path'] );
+			}
+		}
+
+		WP_CLI::line( 'Scan complete' );
+	}
+
+    /**
+     * Adds a repository to the Repo Monitor.
+     *
+     * ## OPTIONS
+     *
+     * <path>
+     * : The path to the repository to add
+	 * <name>
+	 * : The friendly name of the repository
+     *
+     * ## EXAMPLES
+     *
+     *     wp dashboard add_repo --name=Quickstart /srv
+	 *     wp dashboard add_repo --svn --name=WordPress /srv/www/wp
+     *
+     * @synopsis <path> [--warn] [--svn] [--name=<name>]
+	 */
+	function add_repo( $args, $assoc_args ) {
+		$type = 'git';
+		if ( $assoc_args['svn'] ) {
+			$type = 'svn';
+		}
+
+		WP_CLI::line( "Adding $type repository {$assoc_args['name']}..." );
+		WP_CLI::line( "Repo path: {$args[0]}" );
+
+		$repo_monitor = $this->load_repo_monitor();
+		
+		if ( ! $repo_monitor ) {
+			return;
+		}
+
+		$result = $repo_monitor->add_repo( array(
+			'repo_type'			 => $type,
+			'repo_path'			 => $args[0],
+			'repo_friendly_name' => $assoc_args['name'],
+			'warn_out_of_date'   => ! $assoc_args['warn'],
+		) );
+
+		if ( is_wp_error( $result ) ) {
+			WP_CLI::error( "error: ");
+			WP_CLI::error( $result->get_error_message() );
+			return;
+		}
+
+		WP_CLI::success( "Repo added with id $result!" );
+	}
+
+	/**
+	 *
+	 * @return RepoMonitor|bool The RepoMonitor plugin or false on failure
+	 */
+	private function load_repo_monitor() {
+		$instance = Quickstart_Dashboard::get_instance();
+		$plugins = $instance->load_plugins();
+		
+		if ( ! isset( $plugins['RepoMonitor'] ) ) {
+			WP_CLI::error( 'Could not find RepoMonitor plugin' );
+			return false;
+		}
+
+		return $plugins['RepoMonitor'];
+	}
 }
