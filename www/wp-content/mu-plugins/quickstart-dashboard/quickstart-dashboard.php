ec76e7af985e7c1c88615a723273339df53281b9
Mobius5150 <mike@michaelblouin.ca>
2014-02-28 11:02:35 -0700

    Adding thickbox to dashboard for AJAX-y goodness.

--- www/wp-content/mu-plugins/quickstart-dashboard/quickstart-dashboard.php
+++ www/wp-content/mu-plugins/quickstart-dashboard/quickstart-dashboard.php
@@ -145,6 +145,7 @@ class Quickstart_Dashboard {
 	function admin_enqueue_scripts() {
 		wp_enqueue_script( 'dashboard' );
 
+		add_thickbox();
 		if ( wp_is_mobile() )
 			wp_enqueue_script( 'jquery-touch-punch' );
 
