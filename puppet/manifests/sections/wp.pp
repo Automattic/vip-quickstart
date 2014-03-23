95fbd3aa2ed73426591e3f12056b84c132eb29b2
Josh Betz <j@joshbetz.com>
2014-03-23 14:04:02 -0500

    Install WordPress with the Vagrant user

--- puppet/manifests/sections/wp.pp
+++ puppet/manifests/sections/wp.pp
@@ -29,6 +29,7 @@ exec { 'wp install /srv/www/wp':
   command => "/usr/bin/wp core multisite-install --url='${quickstart_domain}' --title='${quickstart_domain}' --admin_email='wordpress@${quickstart_domain}' --admin_name='wordpress' --admin_password='wordpress'",
   cwd     => '/srv/www/wp',
   unless  => "test -z ${quickstart_domain}",
+  user    => 'vagrant',
   require => [
     Vcsrepo['/srv/www/wp'],
     Class['wp::cli'],
