78ca87641b9bb6a1f3952a1e713b1de77bad6bb3
Josh Betz <j@joshbetz.com>
2014-05-27 19:15:30 -0500

    Add mobile theme chooser
    Fixes #91

--- www/wp-content/mu-plugins/mobile-theme-bootstrap.php
+++ www/wp-content/mu-plugins/mobile-theme-bootstrap.php
@@ -1,32 +1,101 @@
 <?php
 
-if ( ! defined( 'VIP_CUSTOM_MOBILE_TEMPLATE' ) )
-    define( 'VIP_CUSTOM_MOBILE_TEMPLATE', '' );
+add_filter( 'jetpack_check_mobile', 'wpcom_check_mobile' );
+function wpcom_check_mobile( $check_mobile_result ) {
+	global $current_blog;
 
-if ( ! defined( 'VIP_CUSTOM_MOBILE_STYLESHEET' ) )
-    define( 'VIP_CUSTOM_MOBILE_STYLESHEET', '' );
+	if ( $check_mobile_result && 1 == get_option('wp_mobile_disable') )
+		return false;
 
-add_action( 'jetpack_modules_loaded', function() {
-    if ( '' == VIP_CUSTOM_MOBILE_TEMPLATE || '' == VIP_CUSTOM_MOBILE_STYLESHEET )
-        return;
+	// iPad or Tablet
+	if ( Jetpack_User_Agent_Info::is_ipad() ) {
+		$check_mobile_result = get_option( 'wp_ipad_template' );
+	} elseif ( Jetpack_User_Agent_Info::is_tablet() ) {
+		$check_mobile_result = get_option( 'wp_tablet_template' );
+	}
 
-    if ( ! jetpack_check_mobile() )
-        return;
+	return $check_mobile_result;
+}
 
-    do_action( 'mobile_setup' );
+add_filter( 'jetpack_mobile_stylesheet', 'wpcom_mobile_stylesheet', 10, 2 );
+function wpcom_mobile_stylesheet( $wp_mobile_stylesheet, $theme ) {
+	$wp_mobile_stylesheet = get_option( 'wp_mobile_template' );
 
-    // Remove Minileven's path overrides
-    remove_filter( 'theme_root', 'minileven_theme_root' );
-    remove_filter( 'theme_root_uri', 'minileven_theme_root_uri' );
+	if ( ! $wp_mobile_stylesheet ) {
+		$should_serve_smart_template = jetpack_is_mobile( 'smart' );
 
-    add_filter( 'jetpack_mobile_stylesheet', function( $stylesheet, $theme ) {
-        return VIP_CUSTOM_MOBILE_STYLESHEET;
-    }, 99, 2 );
+		if ( $should_serve_smart_template ) {
+			if ( ! $wp_mobile_stylesheet = get_option( 'wp_mobile_smart_template' ) ) {
+				$wp_mobile_stylesheet = 'pub/minileven';
+			}
+		} else {
+			if ( ! $wp_mobile_stylesheet = get_option( 'wp_mobile_dumb_template' ) ) {
+				$wp_mobile_stylesheet = 'pub/wp-mobile';
+			}
+		}
 
-    add_filter( 'jetpack_mobile_template', function ( $template, $theme ) {
-            return VIP_CUSTOM_MOBILE_TEMPLATE;
-    }, 99, 2 );
-} );
+		// P2 has its iPhone CSS
+		if ( jetpack_is_mobile( 'smart' ) && in_array( $theme, array( 'pub/p2', 'vip/p2vipsupport' ) ) ) {
+			$wp_mobile_stylesheet = $theme;
+		}
+	}
+
+	// iPad or Tablet
+	if ( Jetpack_User_Agent_Info::is_ipad() && get_option( 'wp_ipad_template' ) ) {
+		$wp_mobile_stylesheet = get_option( 'wp_ipad_template' );
+	} elseif ( Jetpack_User_Agent_Info::is_tablet() && get_option( 'wp_tablet_template' ) ) {
+		$wp_mobile_stylesheet = get_option( 'wp_tablet_template' );
+	}
+
+	return $wp_mobile_stylesheet;
+}
+
+add_filter( 'jetpack_mobile_template', 'wpcom_mobile_template', 10, 2 );
+function wpcom_mobile_template( $wp_mobile_template, $theme ) {
+	$wp_mobile_template = get_option( 'wp_mobile_template' );
+
+	if ( ! $wp_mobile_template ) {
+		$should_serve_smart_template = jetpack_is_mobile( 'smart' );
+
+		if ( $should_serve_smart_template ) {
+			if ( ! $wp_mobile_template = get_option( 'wp_mobile_smart_template' ) ) {
+				$wp_mobile_template = 'pub/minileven';
+			}
+		}
+		else {
+			if ( ! ( $wp_mobile_template = get_option( 'wp_mobile_dumb_template' ) ) ) {
+				$wp_mobile_template = 'pub/wp-mobile';
+			}
+		}
+
+		// P2 has its iPhone CSS
+		if ( jetpack_is_mobile( 'smart' ) && 'pub/p2' == $theme ) {
+			$wp_mobile_template = $theme;
+		}
+	}
+
+	// iPad or Tablet
+	if ( Jetpack_User_Agent_Info::is_ipad() && get_option( 'wp_ipad_template' ) ) {
+		$wp_mobile_template = get_option( 'wp_ipad_template' );
+	} elseif ( Jetpack_User_Agent_Info::is_tablet() && get_option( 'wp_tablet_template' ) ) {
+		$wp_mobile_template = get_option( 'wp_tablet_template' );
+	}
+
+	// Is it a child theme?
+	$wp_mobile_template = get_parent_theme( $wp_mobile_template );
+
+	return $wp_mobile_template;
+}
+
+function get_parent_theme( $theme ) {
+	$template = $theme;
+	$theme_data = wp_get_theme( $theme );
+
+	if ( $theme_data->exists() && '' != $theme_data->Template )
+		$template = $theme_data->Template;
+
+	return $template;
+}
 
 add_action( 'mobile_setup', 'vip_maybe_include_functions_mobile_file' );
 function vip_maybe_include_functions_mobile_file() {
