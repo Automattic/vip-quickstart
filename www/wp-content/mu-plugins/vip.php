9d80aed52ab188577775def3a9c6df084a7c9999
Josh Betz <j@joshbetz.com>
2014-05-29 12:09:31 -0500

    Move wpcom things to wpcom.php and vip things to vip.php

--- www/wp-content/mu-plugins/vip.php
+++ www/wp-content/mu-plugins/vip.php
@@ -1,18 +1,5 @@
 <?php
 
-// Add X-hacker header
-add_action( 'send_headers', function() {
-	header( "X-hacker: If you're reading this, you should visit automattic.com/jobs and apply to join the fun, mention this header." );
-});
-
-add_action( 'admin_menu', function() {
-	// Hide Plugins menu
-	remove_menu_page( 'plugins.php' );
-
-	// Hide Permalinks menu
-	remove_submenu_page( 'options-general.php', 'options-permalink.php' );
-});
-
 // Hide Custom Fields metabox
 add_action( 'do_meta_boxes', function() {
 	remove_meta_box( 'postcustom', get_post_type(), 'normal' );
@@ -34,5 +21,3 @@ function vip_scanner_email_to() {
   // Disabled email submission.
   // return 'vip-support@wordpress.com';
 }
-
-add_filter( 'global_terms_enabled', '__return_true' );
