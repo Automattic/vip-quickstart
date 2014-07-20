806332bd11892dfee8c313b9bf4e5c930807ce96
Josh Betz <j@joshbetz.com>
2014-07-20 12:18:28 -0500

    Make sure we're on the latest Subversion in Debian

--- puppet/manifests/sections/subversion.pp
+++ puppet/manifests/sections/subversion.pp
@@ -3,33 +3,18 @@ class { 'svnupgrade': stage => svnupgrade }
 
 # svnupgrade
 class svnupgrade {
-  line { 'deb svn':
-    file   => '/etc/apt/sources.list',
-    line   => 'deb http://ppa.launchpad.net/svn/ppa/ubuntu precise main',
-    notify => Exec['apt-get update svn']
-  }
-  line { 'deb-src svn':
-    file   => '/etc/apt/sources.list',
-    line   => 'deb-src http://ppa.launchpad.net/svn/ppa/ubuntu precise main',
-    notify => Exec['apt-get update svn']
-  }
+  include apt
 
-  exec { 'apt-get update svn':
-    command     => 'sudo apt-get update',
-    refreshonly => true,
-    require     => [
-      Line['deb svn'],
-      Line['deb-src svn']
-    ]
+  apt::source { 'wheezy-backports':
+    location => 'http://http.debian.net/debian',
+    release  => 'wheezy-backports',
+    repos    => 'main',
   }
 
   exec { 'svn':
-    command => 'sudo apt-get install subversion --yes --force-yes',
-    unless  => 'sudo apt-get install subversion | grep "0 upgraded, 0 newly installed"',
-    require => [
-      Line['deb svn'],
-      Line['deb-src svn'],
-      Exec['apt-get update svn']
-    ]
+    command => 'sudo apt-get install -t wheezy-backports subversion --yes --force-yes',
+    unless  => 'sudo apt-get install -t wheezy-backports subversion | grep "0 upgraded, 0 newly installed"',
+    require => Apt::Source['wheezy-backports'],
   }
 }
+
