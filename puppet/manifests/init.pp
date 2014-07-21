0ec7d46c53370f28a5999da32af26241e7cd8803
Josh Betz <j@joshbetz.com>
2014-07-20 21:58:42 -0500

    Clean up manifests

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -27,3 +27,24 @@ user { 'vagrant':
     system => true,
     notify => Service['php5-fpm'],
 }
+
+# Update Subversion
+stage { 'svnupgrade': before => Stage['main'] }
+class { 'svnupgrade': stage => svnupgrade }
+
+# svnupgrade
+class svnupgrade {
+  include apt
+
+  apt::source { 'wheezy-backports':
+    location => 'http://http.debian.net/debian',
+    release  => 'wheezy-backports',
+    repos    => 'main',
+  }
+
+  exec { 'svn':
+    command => 'sudo apt-get install -t wheezy-backports subversion --yes --force-yes',
+    unless  => 'sudo apt-get install -t wheezy-backports subversion | grep "0 upgraded, 0 newly installed"',
+    require => Apt::Source['wheezy-backports'],
+  }
+}
