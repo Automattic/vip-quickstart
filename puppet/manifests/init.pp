7ebe6984afcd851d29a1c0240ea9689bda3fc87c
Josh Betz <j@joshbetz.com>
2014-07-20 23:11:51 -0500

    Move SVN update (via backports) to updates stage

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -9,17 +9,31 @@ class { 'updates': stage => updates }
 
 # updates
 class updates {
-    exec { 'apt-get update':
-        command => 'apt-get update --quiet --yes',
-        timeout => 0
-    }
+  include apt
 
-    if 'virtualbox' != $virtual {
-        exec { 'apt-get upgrade':
-            command => 'apt-get upgrade --quiet --yes',
-            timeout => 0
-        }
+  apt::source { 'wheezy-backports':
+    location => 'http://http.debian.net/debian',
+    release  => 'wheezy-backports',
+    repos    => 'main',
+  }
+
+  exec { 'apt-get update':
+    command => 'apt-get update --quiet --yes',
+    timeout => 0
+  }
+
+  exec { 'svn':
+    command => 'sudo apt-get install -t wheezy-backports subversion --yes --force-yes',
+    unless  => 'sudo apt-get install -t wheezy-backports subversion | grep "0 upgraded, 0 newly installed"',
+    require => Apt::Source['wheezy-backports'],
+  }
+
+  if 'virtualbox' != $virtual {
+    exec { 'apt-get upgrade':
+      command => 'apt-get upgrade --quiet --yes',
+      timeout => 0
     }
+  }
 }
 
 # Vagrant user
@@ -28,19 +42,4 @@ user { 'vagrant':
     system => true,
     shell  => '/bin/bash',
     notify => Service['php5-fpm'],
-}
-
-# Update SVN
-include apt
-
-apt::source { 'wheezy-backports':
-  location => 'http://http.debian.net/debian',
-  release  => 'wheezy-backports',
-  repos    => 'main',
-}
-
-exec { 'svn':
-  command => 'sudo apt-get install -t wheezy-backports subversion --yes --force-yes',
-  unless  => 'sudo apt-get install -t wheezy-backports subversion | grep "0 upgraded, 0 newly installed"',
-  require => Apt::Source['wheezy-backports'],
 }
