d43d7851ea48bfa57209d1e232bb0893ead1ec48
Josh Betz <j@joshbetz.com>
2014-07-22 23:14:01 -0500

    Fix repomonitor_repo resource
    * We need to pass a path
    * We need to allow the command to be run by a root user
    * We don't need the cwd to be changed

--- puppet/manifests/helpers/repomonitor_repo.pp
+++ puppet/manifests/helpers/repomonitor_repo.pp
@@ -1,11 +1,8 @@
 # Adds a version control repo to the dashboard RepoMonitor plugin so that users are notified when it is out of date.
 define repomonitor_repo ( $repo_name, $path = $title ) {
   exec { "Setup repomonitor ${title}":
-    command => "/usr/bin/wp dashboard add_repo \"${repo_name}\" $path --autodetect",
-    cwd     => '/srv/www/wp-content',
-    unless  => '/usr/bin/wp dashboard list_repos | grep "$path$"',
-    require => [
-      Exec['wp install /srv/www/wp'],
-    ]
+    command   => "/usr/bin/wp dashboard add_repo '${repo_name}' ${path} --autodetect --url=vip.dev --path=/srv/www/wp --allow-root",
+    unless    => "/usr/bin/wp dashboard list_repos | /bin/grep '${path}$' --url=vip.dev --path=/srv/www/wp --allow-root",
+    require   => [ Class['wp::cli'], Exec['wp install /srv/www/wp'] ],
   }
 }
