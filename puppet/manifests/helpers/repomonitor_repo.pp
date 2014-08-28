de035149ab4b495d91c27346bcb93fc3618ef19b
Josh Betz <j@joshbetz.com>
2014-08-28 13:40:56 -0400

    Rename `wp dashboard` to `wp quickstart`

--- puppet/manifests/helpers/repomonitor_repo.pp
+++ puppet/manifests/helpers/repomonitor_repo.pp
@@ -1,14 +1,14 @@
 # Adds a version control repo to the dashboard RepoMonitor plugin so that users are notified when it is out of date.
 define repomonitor_repo ( $repo_name, $path = $title, $type = 'auto' ) {
   if ( $type ) {
-    $command = "/usr/bin/wp dashboard add_repo '${repo_name}' ${path} --type=${type}"
+    $command = "/usr/bin/wp quickstart add_repo '${repo_name}' ${path} --type=${type}"
   } else {
-    $command = "/usr/bin/wp dashboard add_repo '${repo_name}' ${path}"
+    $command = "/usr/bin/wp quickstart add_repo '${repo_name}' ${path}"
   }
 
   exec { "Setup repomonitor ${title}":
     command   => $command,
-    unless    => "/usr/bin/wp dashboard list_repos | /bin/grep '${path}$'",
+    unless    => "/usr/bin/wp quickstart list_repos | /bin/grep '${path}$'",
     require   => [
       Class['wp::cli'],
       Exec['wp install /srv/www/wp']
