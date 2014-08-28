409b173a16ac27dbf63bdc6711a481b75398b7e9
Josh Betz <j@joshbetz.com>
2014-08-28 12:13:37 -0400

    Run repo monitor setup as Vagrant user.

--- puppet/manifests/helpers/repomonitor_repo.pp
+++ puppet/manifests/helpers/repomonitor_repo.pp
@@ -1,8 +1,12 @@
 # Adds a version control repo to the dashboard RepoMonitor plugin so that users are notified when it is out of date.
 define repomonitor_repo ( $repo_name, $path = $title ) {
   exec { "Setup repomonitor ${title}":
-    command   => "/usr/bin/wp dashboard add_repo '${repo_name}' ${path} --autodetect --url=vip.dev --path=/srv/www/wp --allow-root",
-    unless    => "/usr/bin/wp dashboard list_repos | /bin/grep '${path}$' --url=vip.dev --path=/srv/www/wp --allow-root",
-    require   => [ Class['wp::cli'], Exec['wp install /srv/www/wp'] ],
+    command   => "/usr/bin/wp dashboard add_repo '${repo_name}' ${path} --autodetect",
+    unless    => "/usr/bin/wp dashboard list_repos | /bin/grep '${path}$'",
+    require   => [
+      Class['wp::cli'],
+      Exec['wp install /srv/www/wp']
+    ],
+    user => 'vagrant',
   }
 }
