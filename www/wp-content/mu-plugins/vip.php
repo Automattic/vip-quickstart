72b8917b984d88564dd5d909773817c416c7e584
Daniel Bachhuber <daniel@handbuilt.co>
2014-09-03 20:18:00 -0700

    Disable upload space check
    Isn't set by default because of https://github.com/wp-cli/wp-cli/issues/1371

--- www/wp-content/mu-plugins/vip.php
+++ www/wp-content/mu-plugins/vip.php
@@ -10,6 +10,11 @@ add_filter( 'upload_size_limit', function() {
 	return 1073741824; // pow( 2, 30 )
 });
 
+// No upload limit for VIPs
+add_filter( 'pre_site_option_upload_space_check_disabled', function(){
+	return 1;
+});
+
 // Use VIP Theme Review by default
 add_filter( 'vip_scanner_default_review', function( $default, $review_types ) {
   return array_search( 'VIP Theme Review', $review_types );
