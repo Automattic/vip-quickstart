177ec662038654eb37c04f31919f192cde64e157
Josh Betz <j@joshbetz.com>
2014-09-12 15:47:23 -0500

    Repo Monitor: Set default type to blank.
    The CLI command autodetects what type of repo it is unless we
    set one, but 'auto' isn't a valid repo type.

--- puppet/manifests/helpers/repomonitor_repo.pp
+++ puppet/manifests/helpers/repomonitor_repo.pp
@@ -1,5 +1,5 @@
 # Adds a version control repo to the dashboard RepoMonitor plugin so that users are notified when it is out of date.
-define repomonitor_repo ( $repo_name, $path = $title, $type = 'auto' ) {
+define repomonitor_repo ( $repo_name, $path = $title, $type = '' ) {
   if ( $type ) {
     $command = "/usr/bin/wp quickstart add_repo '${repo_name}' ${path} --type=${type}"
   } else {
