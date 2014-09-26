6555df94f41d02d30899cf8c8d01eb1c3b798407
Josh Betz <j@joshbetz.com>
2014-09-26 12:22:51 -0500

    Concat: Only enable concat for subdomain installs
    Concat breaks for subdirectory installs, so let's make sure
    it's only enabled for subdomains. In the future, all
    Quickstart installs will use subdomains anyway.
    
    Fixes #254

--- www/wp-content/mu-plugins/wpcom.php
+++ www/wp-content/mu-plugins/wpcom.php
@@ -1,8 +1,10 @@
 <?php
 
-if ( ! defined( 'QUICKSTART_DISABLE_CONCAT' ) || ! QUICKSTART_DISABLE_CONCAT ) {
-    require __DIR__ . '/http-concat/cssconcat.php';
-    require __DIR__ . '/http-concat/jsconcat.php';
+if ( defined( 'SUBDOMAIN_INSTALL' ) && SUBDOMAIN_INSTALL ) {
+    if ( ! defined( 'QUICKSTART_DISABLE_CONCAT' ) || ! QUICKSTART_DISABLE_CONCAT ) {
+	require __DIR__ . '/http-concat/cssconcat.php';
+	require __DIR__ . '/http-concat/jsconcat.php';
+    }
 }
 
 // Add X-hacker header
