322cd3401a8ad4fefbafea3414c61f7070d58baa
Hau Vong <hvong@pmc.com>
2014-10-07 10:32:40 -0700

    improved scripts

---
+++ pmc/setup-sites.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd `dirname "$0"`
+./bitbucket-gen-key.sh
+
+export HTTP_USER_AGENT="WP_CLI"
+export HTTP_HOST="vip.dev"
+
+while IFS=$',\n\r' read site_slug site_name site_theme
+do           
+	[[ $site_slug = \#* ]] && continue
+	repo=${site_theme}
+	echo "Setting up site: ${site_slug}"
+	
+	if [ ! -d "/srv/www/wp-content/themes/vip/${site_theme}" ]
+	then
+		printf "\nDownloading $repo theme...\n"
+		if [ ! -f /home/vagrant/.ssh/bitbucket.org_id_rsa.pub ]
+		then
+			printf "\nSkipping this step, SSH key has not been created.\n"
+		else
+			git clone git@bitbucket.org:penskemediacorp/${repo}.git /srv/www/wp-content/themes/vip/${site_theme}
+		fi
+	fi
+	
+	STATUS=`/usr/bin/wp --path=/srv/www/wp site list | grep /${site_slug}/`
+	if [ "" == "${STATUS}" ]; then
+		/usr/bin/wp --path=/srv/www/wp site create --slug=${site_slug} --title=${site_theme}
+	fi
+	
+	STATUS=`/usr/bin/wp --path=/srv/www/wp --url=vip.dev/${site_slug} theme status | grep "A vip/${site_theme} "`
+	if [ "" == "${STATUS}" ]; then
+		/usr/bin/wp --path=/srv/www/wp --url=vip.dev/${site_slug} theme activate vip/${site_theme}
+	fi
+	
+done < ./sites
