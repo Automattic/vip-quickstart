1fa3aed4cdec8da6b384147c8f227a8584b52251
Hau Vong <hvong@pmc.com>
2014-10-10 11:18:15 -0700

    update pmc mu plugin

--- www/wp-content/mu-plugins/pmc.php
+++ www/wp-content/mu-plugins/pmc.php
@@ -16,3 +16,14 @@ add_filter( 'pre_site_transient_update_core', '__return_null' );
 if ( !isset ( $GLOBALS['pagenow'] ) ) {
 	$GLOBALS['pagenow'] = '';
 }
+
+/**
+ * Alter the timeout on cron requests from 0.01 to 0.5. Something about
+ * the Vagrant and/or Ubuntu setup doesn't like these self requests 
+ * happening so quickly.
+ */
+add_filter( 'cron_request', 'jf_cron_request', 10, 1 );
+function jf_cron_request( $cron_request ) {
+	$cron_request['args']['timeout'] = (float) 0.5;
+	return $cron_request;
+}
