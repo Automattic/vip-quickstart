a5c8a4642e47433bf71903ead51555e4c48ed70b
Hau Vong <hvong@pmc.com>
2014-10-12 13:06:37 -0700

    use local.dev domain

--- pmc/setup-sites.sh
+++ pmc/setup-sites.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DOMAIN='local.dev'
+
 cd `dirname "$0"`
 
 # workaround pmc_analystics required this theme to be at this location.
@@ -11,13 +13,13 @@ if [ ! -f ~/.ssh/bitbucket.org_id_rsa ]; then
 	./bitbucket-gen-key.sh
 fi
 
-sed -e '$a\' -e "define('SUBDOMAIN_INSTALL', true );" -e "/define\s*(\s*'SUBDOMAIN_INSTALL'/d" -i /srv/www/local-config.php 
+sed -e '$a\' -e "define('SUBDOMAIN_INSTALL', true );" -e "/define\s*(\s*'SUBDOMAIN_INSTALL'/d" -i /srv/www/local-config.php
 if [ "0" == "`/usr/bin/wp --path=/srv/www/wp network meta get 1 subdomain_install`" ]; then
 	/usr/bin/wp --path=/srv/www/wp network meta update 1 subdomain_install 1
 fi
 
 export HTTP_USER_AGENT="WP_CLI"
-export HTTP_HOST="vip.dev"
+export HTTP_HOST="${DOMAIN}"
 
 if [ ! -d "/srv/www/wp-content/themes/vip/pmc-plugins" ]
 then
@@ -31,16 +33,16 @@ then
 fi
 
 while IFS=$',\n\r' read site_slug site_name site_theme
-do           
+do
 	[[ $site_slug = \#* ]] && continue
-	
+
 	if [ "" != "$1" ]; then
 		[[ "$1" != "${site_slug}" ]] && continue
 	fi
-	
+
 	repo=${site_theme}
 	echo "Setting up site: ${site_slug}"
-	
+
 	if [ ! -d "/srv/www/wp-content/themes/vip/${site_theme}" ]
 	then
 		printf "\nDownloading $repo theme...\n"
@@ -51,18 +53,18 @@ do
 			git clone git@bitbucket.org:penskemediacorp/${repo}.git /srv/www/wp-content/themes/vip/${site_theme}
 		fi
 	fi
-	
-	STATUS=`/usr/bin/wp --path=/srv/www/wp site list --fields=domain --format=csv | grep "${site_slug}.vip.dev"`
+
+	STATUS=`/usr/bin/wp --path=/srv/www/wp site list --fields=domain --format=csv | grep "${site_slug}.${DOMAIN}"`
 	if [ "" == "${STATUS}" ]; then
 		/usr/bin/wp --path=/srv/www/wp site create --slug=${site_slug} --title=${site_name}
 	fi
-	
-	STATUS=`/usr/bin/wp --path=/srv/www/wp --url=variety.vip.dev theme status vip/pmc-variety-2014 | grep 'Status: Inactive'`
+
+	STATUS=`/usr/bin/wp --path=/srv/www/wp --url=${site_slug}.${DOMAIN} theme status vip/${site_theme} | grep 'Status: Inactive'`
 	if [ "" != "${STATUS}" ]; then
 		/usr/bin/wp --path=/srv/www/wp theme enable vip/${site_theme} --network
-		/usr/bin/wp --path=/srv/www/wp --url=${site_slug}.vip.dev theme activate vip/${site_theme}
+		/usr/bin/wp --path=/srv/www/wp --url=${site_slug}.${DOMAIN} theme activate vip/${site_theme}
 	fi
-	
+
 done < ./sites
 
 cp -f /etc/hosts /srv/pmc/vagrant_hosts
@@ -72,7 +74,7 @@ sed -e "/#pmcsetup/d" -i /srv/pmc/vagrant_hosts
 /usr/bin/wp --path=/srv/www/wp site list --fields=domain --format=csv | sed -e 's/^domain$//' -e 's/^\(.\+\)/[ip] \1 #pmcsetup/' > server_hosts
 sed -e 's/\[ip\]/10.86.73.80/' server_hosts >> /srv/pmc/hosts
 
-# need this for wp cron to work 
+# need this for wp cron to work
 sed -e 's/\[ip\]/127.0.0.1/' server_hosts >> /srv/pmc/vagrant_hosts
 sudo cp -f /srv/pmc/vagrant_hosts /etc/hosts
 
@@ -82,7 +84,7 @@ if [ ! -f /etc/ssl/dev-san-domain.key ] || [ ! -f /etc/ssl/dev-san-domain-chaine
 fi
 
 # nginx ssl
-sudo sed -e "/http {/a\ \ ssl_certificate     /etc/ssl/dev-san-domain-chained.crt;\n\ \ ssl_certificate_key /etc/ssl/dev-san-domain.key;" -e '/ssl_certificate/d' -i /etc/nginx/nginx.conf 
-sudo sed -e "/listen 80;/a\ \ listen 443 ssl;" -e "/listen 443/d" -i /etc/nginx/sites-available/50-_.conf  
-sudo sed -e "/listen 80;/a\ \ listen 443 ssl;" -e "/listen 443/d" -i /etc/nginx/sites-enabled/50-_.conf  
+sudo sed -e "/http {/a\ \ ssl_certificate     /etc/ssl/dev-san-domain-chained.crt;\n\ \ ssl_certificate_key /etc/ssl/dev-san-domain.key;" -e '/ssl_certificate/d' -i /etc/nginx/nginx.conf
+sudo sed -e "/listen 80;/a\ \ listen 443 ssl;" -e "/listen 443/d" -i /etc/nginx/sites-available/50-_.conf
+sudo sed -e "/listen 80;/a\ \ listen 443 ssl;" -e "/listen 443/d" -i /etc/nginx/sites-enabled/50-_.conf
 sudo service nginx reload
--- wbin/pmc-init.bat
+++ wbin/pmc-init.bat
@@ -1,9 +1,9 @@
 @echo off
 pushd %~dp0
-call vip-init.bat -domain vip.dev
+call vip-init.bat -domain local.dev
 cd ..
 copy /Y %SystemRoot%\System32\drivers\etc\hosts pmc\hosts.backup
 copy /Y %SystemRoot%\System32\drivers\etc\hosts pmc
 vagrant ssh -c /srv/pmc/setup-sites.sh
-copy pmc\hosts /Y %SystemRoot%\System32\drivers\etc 
+copy pmc\hosts /Y %SystemRoot%\System32\drivers\etc
 popd
