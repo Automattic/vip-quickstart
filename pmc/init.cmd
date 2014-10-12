926ce29c56969ffef2a61b60792cb6c9b3653e56
Hau Vong <hvong@pmc.com>
2014-10-12 15:45:59 -0700

    Add missing file

---
+++ pmc/init.cmd
@@ -0,0 +1,8 @@
+@echo off
+pushd %~dp0\..
+vagrant up %1
+copy /Y %SystemRoot%\System32\drivers\etc\hosts pmc\hosts.backup > nul
+copy /Y %SystemRoot%\System32\drivers\etc\hosts pmc > nul
+vagrant ssh -c /srv/pmc/setup-sites.sh
+copy pmc\hosts /Y %SystemRoot%\System32\drivers\etc  > nul
+popd
