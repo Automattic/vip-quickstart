f6cb580b5f0c01d0c5b4f703814fef75b181b5c6
Weston Ruter <weston@xwp.co>
2014-10-18 00:09:03 -0700

    Make wp-config.php conform to WP coding standards

--- www/wp-config.php
+++ www/wp-config.php
@@ -3,8 +3,9 @@
 /**
  * Local mods
  */
-if ( file_exists( __DIR__ . '/local-config.php' ) )
-    require __DIR__ . '/local-config.php';
+if ( file_exists( __DIR__ . '/local-config.php' ) ) {
+	require __DIR__ . '/local-config.php';
+}
 
 /**
  * The base configurations of the WordPress.
@@ -23,25 +24,28 @@ if ( file_exists( __DIR__ . '/local-config.php' ) )
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-if ( ! defined( 'DB_NAME' ) )
-    define('DB_NAME', 'wordpress');
+if ( ! defined( 'DB_NAME' ) ) {
+	define( 'DB_NAME', 'wordpress' );
+}
 
 /** MySQL database username */
-if ( ! defined( 'DB_USER' ) )
-    define('DB_USER', 'wordpress');
+if ( ! defined( 'DB_USER' ) ) {
+	define( 'DB_USER', 'wordpress' );
+}
 
 /** MySQL database password */
-if ( ! defined( 'DB_PASSWORD' ) )
-    define('DB_PASSWORD', 'wordpress');
+if ( ! defined( 'DB_PASSWORD' ) ) {
+	define( 'DB_PASSWORD', 'wordpress' );
+}
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define( 'DB_COLLATE', '' );
 
 /**#@-*/
 
@@ -51,7 +55,7 @@ define('DB_COLLATE', '');
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix = 'wp_';
 
 /**
  * WordPress Localized Language, defaults to English.
@@ -61,8 +65,9 @@ $table_prefix  = 'wp_';
  * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
  * language support.
  */
-if ( ! defined( 'WPLANG' ) )
-    define('WPLANG', '');
+if ( ! defined( 'WPLANG' ) ) {
+	define( 'WPLANG', '' );
+}
 
 /**
  * For developers: WordPress debugging mode.
@@ -71,33 +76,38 @@ if ( ! defined( 'WPLANG' ) )
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', true);
-define('SAVEQUERIES', true);
+define( 'WP_DEBUG', true );
+define( 'SAVEQUERIES', true );
 
-if ( ! defined( 'JETPACK_DEV_DEBUG' ) )
-    define('JETPACK_DEV_DEBUG', true);
+if ( ! defined( 'JETPACK_DEV_DEBUG' ) ) {
+	define( 'JETPACK_DEV_DEBUG', true );
+}
 
-if ( ! defined( 'MP6_STYLE_GUIDE' ) )
-    define('MP6_STYLE_GUIDE', true);
+if ( ! defined( 'MP6_STYLE_GUIDE' ) ) {
+	define( 'MP6_STYLE_GUIDE', true );
+}
 
 /* Content Directory */
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
 
-define('MULTISITE', true);
-define('SUNRISE', true);
-define('PATH_CURRENT_SITE', '/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
+define( 'MULTISITE', true );
+define( 'SUNRISE', true );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
 
-if ( ! defined( 'DOMAIN_CURRENT_SITE' ) )
-    define('DOMAIN_CURRENT_SITE', $_SERVER['HTTP_HOST']);
+if ( ! defined( 'DOMAIN_CURRENT_SITE' ) ) {
+	define( 'DOMAIN_CURRENT_SITE', $_SERVER['HTTP_HOST'] );
+}
 
-if ( ! defined( 'SUBDOMAIN_INSTALL' ) )
-    define('SUBDOMAIN_INSTALL', false);
+if ( ! defined( 'SUBDOMAIN_INSTALL' ) ) {
+	define( 'SUBDOMAIN_INSTALL', false );
+}
 
-if ( ! defined( 'WP_DEFAULT_THEME' ) )
-	define('WP_DEFAULT_THEME', 'pub/twentyfourteen');
+if ( ! defined( 'WP_DEFAULT_THEME' ) ) {
+	define( 'WP_DEFAULT_THEME', 'pub/twentyfourteen' );
+}
 
 /* That's all, stop editing! Happy blogging. */
 
@@ -113,8 +123,9 @@ require __DIR__ . '/config/roles.php';
 require __DIR__ . '/config/vip-config.php';
 
 /** Absolute path to the WordPress directory. */
-if ( !defined( 'ABSPATH' ) )
+if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once( ABSPATH . 'wp-settings.php' );
