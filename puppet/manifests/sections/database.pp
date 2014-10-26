38e10cd0e0d13c9605b56e1b390ec4ffe2ccc0c6
Chris Hardie <chris@summersault.com>
2014-10-25 21:45:42 -0400

    Revert "Use a random DB password, to fix https://github.com/Automattic/vip-quickstart/issues/126"
    This reverts commit 9451ee193428b391ade5cb8d4e66485f3e0674a5.

--- puppet/manifests/sections/database.pp
+++ puppet/manifests/sections/database.pp
@@ -1,13 +1,6 @@
-
-class database::settings {
-    $mysql_password = generate('/bin/sh', '-c', '/usr/bin/openssl rand -base64 64 | xargs echo -n')
-}
-
-include database::settings
-
 mysql::grant { 'wordpress':
   mysql_privileges => 'ALL',
-  mysql_password   => $database::settings::mysql_password,
+  mysql_password   => 'wordpress',
   mysql_db         => 'wordpress',
   mysql_user       => 'wordpress',
   mysql_host       => 'localhost',
@@ -15,9 +8,8 @@ mysql::grant { 'wordpress':
 
 mysql::grant { 'wptests':
   mysql_privileges => 'ALL',
-  mysql_password   => $database::settings::mysql_password,
+  mysql_password   => 'wptests',
   mysql_db         => 'wptests',
   mysql_user       => 'wptests',
   mysql_host       => 'localhost',
 }
-
