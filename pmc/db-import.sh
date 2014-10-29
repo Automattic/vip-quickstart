99f95738ae8591193f593b8bc36d9f5449d6c0e1
Hau Vong <hvong@pmc.com>
2014-10-29 14:11:41 -0700

    Fix import script - add footwearnews

--- pmc/db-import.sh
+++ pmc/db-import.sh
@@ -19,7 +19,7 @@ site_id=`/usr/bin/wp --path=/srv/www/wp site list --fields=blog_id,domain --form
 if [ "" != "${site_id}" ]
 then
 	echo "Importing ${sql_file} into ${site_slug} site id ${site_id}"
-	sed -e "s/CREATE TABLE \`wp_/CREATE TABLE \`wp_${site_id}_/g" -e "s/DROP TABLE IF EXISTS \`wp_/DROP TABLE IF EXISTS \`wp_${site_id}_/g" ${sql_file} | mysql -uroot wordpress
+	sed -e "s/ INTO \`wp_/ INTO \`wp_${site_id}_/g" -e "s/CREATE TABLE \`wp_/CREATE TABLE \`wp_${site_id}_/g" -e "s/DROP TABLE IF EXISTS \`wp_/DROP TABLE IF EXISTS \`wp_${site_id}_/g" ${sql_file} | mysql -uroot wordpress
 	/usr/bin/wp --path=/srv/www/wp --require=/srv/pmc/pmc-wp-cli.php  pmc-site fix "${site_slug}.local.dev" --title="${site_slug}"
 else
 	echo 'Site not found'
