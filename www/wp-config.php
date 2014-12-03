a91d3921c7789c1d420dc7d386f6fcc3d55cb9de
Weston Ruter <weston@xwp.co>
2014-12-03 12:59:46 +1100

    Improve WP_DEBUG instruction comment

--- www/wp-config.php
+++ www/wp-config.php
@@ -75,9 +75,10 @@ if ( ! defined( 'WPLANG' ) ) {
 /**
  * For developers: WordPress debugging mode.
  *
- * Change this to true to enable the display of notices during development.
+ * When WP_DEBUG is true, PHP notices will be displayed during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
+ * in their development environments. On a staging environment, this may be
+ * overridden to false via local-config.php
  */
 if ( ! defined( 'WP_DEBUG' ) ) {
 	define( 'WP_DEBUG', true );
