db7509f146bfbe3b3304c978f77da5a3da9c4113
Josh Betz <j@joshbetz.com>
2014-12-07 15:15:30 -0600

    Clean up wp-config

--- www/wp-config.php
+++ www/wp-config.php
@@ -7,44 +7,20 @@ if ( file_exists( __DIR__ . '/local-config.php' ) ) {
 	require __DIR__ . '/local-config.php';
 }
 
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
-
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
 if ( ! defined( 'DB_NAME' ) ) {
 	define( 'DB_NAME', 'wordpress' );
 }
 
-/** MySQL database username */
 if ( ! defined( 'DB_USER' ) ) {
 	define( 'DB_USER', 'wordpress' );
 }
 
-/** MySQL database password */
 if ( ! defined( 'DB_PASSWORD' ) ) {
 	define( 'DB_PASSWORD', 'wordpress' );
 }
 
-/** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
-
-/** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
-
-/** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
 /** Disable Automatic core updates. */
