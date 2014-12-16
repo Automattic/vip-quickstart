13236293c6b633267b119924b043741a71cfb8db
Hau Vong <hvong@pmc.com>
2014-12-16 13:29:09 -0800

    restart memcached after db import

--- pmc/db-import.sh
+++ pmc/db-import.sh
@@ -21,6 +21,7 @@ then
 	echo "Importing ${sql_file} into ${site_slug} site id ${site_id}"
 	sed -e "s/CREATE TABLE IF NOT EXISTS \`wp_/CREATE TABLE IF NOT EXISTS \`wp_${site_id}_/g" -e "s/ INTO \`wp_/ INTO \`wp_${site_id}_/g" -e "s/ TABLE \`wp_/ TABLE \`wp_${site_id}_/g" -e "s/table \`wp_/table \`wp_${site_id}_/g" -e "s/LOCK TABLES \`wp_/LOCK TABLES \`wp_${site_id}_/g" -e "s/DROP TABLE IF EXISTS \`wp_/DROP TABLE IF EXISTS \`wp_${site_id}_/g" ${sql_file} | mysql -uroot wordpress
 	/usr/bin/wp --path=/srv/www/wp --require=/srv/pmc/pmc-wp-cli.php  pmc-site fix "${site_slug}.local.dev" --title="${site_slug}"
+	sudo service memcached restart
 else
 	echo 'Site not found'
 fi
