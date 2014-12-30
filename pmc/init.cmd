5567fd668626fff0dfe436223becde892479368b
Hau Vong <hvong@pmc.com>
2014-12-30 13:43:39 -0800

    provision during vagrant up

--- pmc/init.cmd
+++ pmc/init.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd %~dp0\..
-vagrant up %1
+vagrant up --provision %1
 copy /Y %SystemRoot%\System32\drivers\etc\hosts pmc\hosts.backup > nul
 copy /Y %SystemRoot%\System32\drivers\etc\hosts pmc > nul
 vagrant ssh -c /srv/pmc/setup-sites.sh -- -t
