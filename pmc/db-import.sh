c9aab7e8f128be54c21623495e9ee7642bfe11b3
Hau Vong <hvong@pmc.com>
2015-01-08 16:11:38 -0800

    update db import script to work with vip.local domain

--- pmc/db-import.sh
+++ pmc/db-import.sh
@@ -14,13 +14,13 @@ if [ ! -f "${sql_file}" ]; then
 	exit
 fi
 
-site_id=`/usr/bin/wp --path=/srv/www/wp site list --fields=blog_id,domain --format=csv|grep "${site_slug}.local.dev" | cut -d',' -f1`
+site_id=`/usr/bin/wp --path=/srv/www/wp site list --fields=blog_id,domain --format=csv|grep "${site_slug}.vip.local" | cut -d',' -f1`
 
 if [ "" != "${site_id}" ]
 then
 	echo "Importing ${sql_file} into ${site_slug} site id ${site_id}"
 	sed -e "s/CREATE TABLE IF NOT EXISTS \`wp_/CREATE TABLE IF NOT EXISTS \`wp_${site_id}_/g" -e "s/ INTO \`wp_/ INTO \`wp_${site_id}_/g" -e "s/ TABLE \`wp_/ TABLE \`wp_${site_id}_/g" -e "s/table \`wp_/table \`wp_${site_id}_/g" -e "s/LOCK TABLES \`wp_/LOCK TABLES \`wp_${site_id}_/g" -e "s/DROP TABLE IF EXISTS \`wp_/DROP TABLE IF EXISTS \`wp_${site_id}_/g" ${sql_file} | mysql -uroot wordpress
-	/usr/bin/wp --path=/srv/www/wp --require=/srv/pmc/pmc-wp-cli.php  pmc-site fix "${site_slug}.local.dev" --title="${site_slug}"
+	/usr/bin/wp --path=/srv/www/wp --require=/srv/pmc/pmc-wp-cli.php  pmc-site fix "${site_slug}.vip.local" --title="${site_slug}"
 	sudo service memcached restart
 else
 	echo 'Site not found'
