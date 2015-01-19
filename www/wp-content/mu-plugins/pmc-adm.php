2b29500d8f87d7f5c91ec356eac54f839df267d0
Hau Vong <hvong@pmc.com>
2015-01-19 11:35:32 -0800

    Add mu plugin to adjust pmc adm host targetting to corresponding live site for domain name *.vip.local

---
+++ www/wp-content/mu-plugins/pmc-adm.php
@@ -0,0 +1,12 @@
+<?php
+
+// adjust pmc adm host targetting to corresponding live site for domain name *.vip.local
+add_filter( 'pmc_adm_hostname', function() {
+	$parts = explode( '.', $_SERVER['HTTP_HOST'] );	
+	switch ( end( $parts ) ) {
+		case 'local':
+			echo reset( $parts ) .'.com';
+			break;
+	}
+	return $_SERVER['HTTP_HOST'];
+} );
