Automattic/vip-quickstart

commit 7fee8815060d90560dee9558896eb7d2f3283eff

Bump PHP module to fix #386, #289

puppet/manifests/sections/php.pp
@@ -53,8 +53,6 @@ vcsrepo { '/usr/share/php/PHP/CodeSniffer/Standards/WordPress':
   require  => Package['pear.php.net/PHP_CodeSniffer'],
 }
 
-php::fpm::conf { 'www': user => 'www-data' }
-
 # Turn on html_errors
 exec { 'html_errors = On':
   command => 'sed -i "s/html_errors = Off/html_errors = On/g" /etc/php5/fpm/php.ini',

commit 2902bb0a03b226df08c6eb5087f197145741a1ff

Correct PHP-FPM user
This was broken in
https://github.com/Automattic/vip-quickstart/commit/7fee8815060d90560dee9558896eb7d2f3283eff

puppet/manifests/sections/php.pp
@@ -40,6 +40,8 @@ class {
     }
 }
 
+php::fpm::pool { 'www': user => 'www-data' }
+
 # Install PHP_CodeSniffer and the WordPress coding standard
 package { 'pear.php.net/PHP_CodeSniffer':
   ensure   => 'installed',