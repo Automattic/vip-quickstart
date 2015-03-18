4d79efbfffc4c28ca93f51376b6deb292e6474f3
Amit Sannad <asannad@pmc.com>
2015-03-18 23:18:18 +0530

    Add uls.wwd.local

---
+++ pmc/.env.local.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'WWD_DB_HOST'     => 'localhost',
+	'WWD_DB_NAME'     => 'uls_wwd_local',
+	'WWD_DB_USERNAME' => 'root',
+	'WWD_DB_PASSWORD' => '',
+	'APPLICATION_ENV' => 'local'
+);
+
+//EOF
